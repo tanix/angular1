e3de981952e359cd71c598b852416e9ac340459d
Tatsiana_Tratsinnikava@epam.com <Tatsiana_Tratsinnikava@epam.com>
2017-09-06 12:25:55 +0300

    Styling was added to todo items table.

--- js/example.js
+++ js/example.js
@@ -1,60 +1,69 @@
 angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
-  var $ctrl = this;
+	var $ctrl = this;
+	$ctrl.search = '';
 
-  $ctrl.items  = [
+	$ctrl.items  = [
 		{
 			title: 'Задача 1',
 			description: 'Описание задачи 1',
-			status: 'To do'
+			status: true
 		},
 		{
 			title: 'Задача 2',
 			description: 'Описание задачи 2',
-			status: 'Done'
+			status: false
 		},
 		{
 			title: 'Задача 3',
 			description: 'Описание задачи 3',
-			status: 'To do'
+			status: false
 		},
 	];
 
-	$ctrl.search = '';
-
-  $ctrl.open = function (item) {
-  	
-    var modalInstance = $uibModal.open({
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      controllerAs: '$ctrl',    
-      resolve: {
-        item: function () {
-          return item;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (data) {
-      		$ctrl.items.push(
-				{
-					title: data.title,
-					description: data.description,
-					status: ''
-				}
-			);
-			
-			
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  	$ctrl.remove = function(item) {
+	$ctrl.remove = function(item) {
 		var index = $ctrl.items.indexOf(item);
-		$ctrl.items.splice(index, 1);		
+		$ctrl.items.splice(index, 1);
+	};
+
+	$ctrl.update = function(item) {
+	};
+
+	$ctrl.add = function(item) {
+	};
+
+	$ctrl.open = function(item) {
+		var modalInstance = $uibModal.open({
+		ariaLabelledBy: 'modal-title',
+		ariaDescribedBy: 'modal-body',
+		templateUrl: 'myModalContent.html',
+		controller: 'ModalInstanceCtrl',
+		controllerAs: '$ctrl',
+		resolve: {
+			item: function () {
+				return item;
+			}
+		}
+	});
+
+	modalInstance.result.then(function (data) {
+		// $ctrl.items.push(
+		// 	{
+		// 		title: data.title,
+		// 		description: data.description,
+		// 		status: ''
+		// 	}
+		// );
+
+		var index = $ctrl.items.indexOf(item);
+			$ctrl.items.splice(index, 1, 				{
+			title: data.title,
+			description: data.description,
+			status: ''});
+
+		}, function () {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
 	};
 });
 
@@ -80,12 +89,12 @@ angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($u
 
 
 
-  $ctrl.ok = function () {
-    $uibModalInstance.close($ctrl.newItem);
-  };
+	$ctrl.ok = function () {
+		$uibModalInstance.close($ctrl.newItem);
+	};
 
-  $ctrl.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
+	$ctrl.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
 });
 
